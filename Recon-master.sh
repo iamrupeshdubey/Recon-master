8032198da1a44a1fa5ad5832d6e356d2282919d0
Rupesh Dubey <40837950+iamrupeshdubey@users.noreply.github.com>
2019-08-04 21:29:23 +0530

    Added support of scraping js files of a website.

--- Recon-master.sh
+++ Recon-master.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo -n "Enter domain you want to recon: "
 read DOMAIN
+mkdir -p recons&&cd recons
+mkdir -p $DOMAIN-recon&& cd $DOMAIN-recon
 assetfinder $DOMAIN | tee $DOMAIN.txt
 cat $DOMAIN.txt | sort -u | while read line; do
     if [ $(curl --write-out %{http_code} --silent --output /dev/null -m 5 $line) = 000 ]
@@ -14,8 +16,10 @@ cat $DOMAIN.txt | sort -u | while read line; do
     fi
   done
 
-awk '{print "https://"$0}' $DOMAIN-responsive.txt > $DOMAIN-recon
-sed -i 's/www.//' $DOMAIN-recon
-sort -u $DOMAIN-recon
-echo -e "\e[31mRunning directory buster now\e[0m"
-for i in `cat $DOMAIN-recon`; do dirb $i -o $DOMAIN-dirb;done
+awk '{print "https://"$0}' $DOMAIN-responsive.txt | sed -i 's/www.//' $DOMAIN-responsive.txt | sort -u $DOMAIN-responsive.txt > $DOMAIN-recon
+echo -e "\e[31mFinding js files in $DOMAIN\e[0m"
+cat $DOMAIN-recon | waybackurls | grep "\.js" | sort -u | tee $DOMAIN-temp
+cat $DOMAIN-temp | parallel -j50 -q curl -w 'Status:%{http_code}\t Size:%{size_download}\t %{url_effective}\n' -o /dev/null -sk| grep -v 404 | grep -v 429 | grep -v 000 | tee $DOMAIN-js
+rm -rf $DOMAIN.txt $DOMAIN-responsive.txt $DOMAIN-temp
+#echo -e "\e[31mRunning directory buster now\e[0m"
+#for i in `cat $DOMAIN-recon`; do dirb $i -o $DOMAIN-dirb;done
